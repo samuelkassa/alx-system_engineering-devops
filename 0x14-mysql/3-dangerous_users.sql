3d7fe06d8a7ab2c186b250eea3737cc9608cdba8
= <=>
2023-09-22 21:13:12 +0000

    Making replication

--- 0x14-mysql/3-dangerous_users.sql
+++ 0x14-mysql/3-dangerous_users.sql
@@ -1,5 +1,5 @@
 -- unless we like to live dangerously, in which case we will ->
-CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'hbtn89';
+CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'alem27';
 GRANT REPLICATION CLIENT, REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
 GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
--- 0x14-mysql/6-master.sql
+++ 0x14-mysql/6-master.sql
@@ -1,9 +1,9 @@
 -- lets change the slave to be synchronized with the server
 CHANGE MASTER TO
-MASTER_HOST='54.146.84.110',
+MASTER_HOST='54.237.110.17',
 MASTER_USER='replica_user',
-MASTER_PASSWORD='hbtn89',
-MASTER_LOG_FILE='mysql-bin.000001',
-MASTER_LOG_POS=727;
+MASTER_PASSWORD='alem27',
+MASTER_LOG_FILE='mysql_bin_log.000003',
+MASTER_LOG_POS=154;
 
 START SLAVE;
